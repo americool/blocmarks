f488578746dd0c20231711af4629cd257b8f05a0
Abe Anderson <aberanderson@gmail.com>
2016-03-04 12:37:07 -0500

    starting out new branch thing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,15 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  
+  config.action_mailer.raise_delivery_errors = true
+
+
+
+
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
