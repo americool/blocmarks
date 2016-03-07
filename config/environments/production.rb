d6aed4222d5c86ee35c289ac6610287aa0741d2f
Abe Anderson <aberanderson@gmail.com>
2016-03-07 17:41:25 -0500

    trying to get it work w/ heroku crap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'peaceful-mesa-61462.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'floating-brook-16711.herokuapp.com'}
 
   config.action_mailer.delivery_method = :smtp
 
