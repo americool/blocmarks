ca0cf1854cba4ddd47de573aac86acc4ede77204
Abe Anderson <aberanderson@gmail.com>
2016-03-07 09:53:07 -0500

    current status

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+
+  after_create :send_confirmation
+
+  private
+
+  def send_confirmation
+    self.send_confirmation_instructions
+  end
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,9 +7,20 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost' }
   config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.smtp_settings = {
+    address:                   "smtp.sendgrid.net",
+    port:                      587,
+    domain:                    "sendgrid.com",
+    user_name:                 ENV['SENDGRID_USERNAME'],
+    password:                  ENV['SENDGRID_PASSWORD'],
+    authentication:           'plain',
+    enable_starttls_auto:      true  
+}
 
 
   config.cache_classes = false
