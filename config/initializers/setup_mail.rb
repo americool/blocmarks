6ae5c12aaf87e7fa96617691bf0a38ceb3b95098
Abe Anderson <aberanderson@gmail.com>
2016-03-07 17:09:26 -0500

    trying to destroy sendgrid to start over

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,14 @@
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
 
-  address:         'smtp.sendgrid.net',
-  port:            '587',
-  authentication:   :plain,
-  user_name:        ENV['SENDGRID_USERNAME'],
-  password:         ENV['SENDGRID_PASSWORD'],
-  domain:           'heroku.com',
-  enable_starttls_auto:  true
-}
+if Rails.env.development? || Rails.env.production?
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+
+    address:         'smtp.gmail.com',
+    port:            '587',
+    authentication:   :plain,
+    user_name:        "aberanderson@gmail.com",
+    password:         "dogsondogs85",
+    domain:           'gmail.com',
+    enable_starttls_auto:  true
+  }
+end
